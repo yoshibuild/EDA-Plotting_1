e6e20d0c5ac005b45a3c235ca70473369c44386b
yoshibuild <yoshibuild@gmail.com>
2014-05-11 10:28:19 +0900

    rev1

--- Plot1.R
+++ Plot1.R
@@ -5,10 +5,8 @@ x<- as.numeric(as.vector(dat_obj$Global_active_power))
 
 with(dat_obj, hist(x, main="Global Activity Power", xlab="Global Active Power (kilowatts)", col ="red"))
 
-png("Plot1.png", width=480, height=480)
-with(dat_obj, hist(x, main="Global Activity Power", xlab="Global Active Power (kilowatts)", col ="red"))
+dev.copy(png, file="Plot1.png", width=480, height=480)
 dev.off()
 
-#dev.copy(png, file="plot1.png")
-#dev.off()
+
 
